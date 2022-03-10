179a9ddcf6fab370e79e6a94740cbc7bcf5c69ae
Juliana Sau <julienyange@gmail.com>
2022-03-10 10:27:38 -0800

    Create C program to print string and return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a quote without forbidden functions
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is userful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
