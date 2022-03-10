90b9249bc3204996c70d4fcc73aaddb4b19e4743
Juliana Sau <julienyange@gmail.com>
2022-03-10 07:15:45 -0800

    Create a C file that prints a string and returns 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program that prints "Programming is like building a multilingual puzzle" and returns 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}    
