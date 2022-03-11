72f29da6f652d85762a2ab70a66978fe58efe790
Juliana Sau <julienyange@gmail.com>
2022-03-11 11:26:43 -0800

    Create program that prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints out alphabet in lower case
+ *
+ * Description: prints out alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
