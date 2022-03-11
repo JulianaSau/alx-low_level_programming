f5e85f6d4450639d807f92574c2b2bb499ddbef1
Juliana Sau <julienyange@gmail.com>
2022-03-11 12:08:43 -0800

    Create program that prints alphabet in upper and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,7 +15,7 @@ int main(void)
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 
-	for (alphabet = 'A'; alphabet<= 'Z'; alphabet++)
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		putchar(alphabet);
 
 	putchar('\n');
