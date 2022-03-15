2e76fe738e92651ea78031d08afb7cb9d72527f5
Juliana Sau <julienyange@gmail.com>
2022-03-15 09:24:30 -0700

    Create program to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two given numbers
+ *
+ * @num1: paramenter 1
+ * @num2: parameter 2
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
