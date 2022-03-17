8fba30e6037fe8b1efe9b85fa122c62fd7ca632a
Sau Juliana <julienyange@gmail.com>
2022-03-18 00:28:29 +0300

    Add program to print a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,29 @@
 #include "main.h"
 
 /**
- * print_line - prints a diagonal line followed by a new line
+ * print_diagonal - prints diagonal line
  *
  * @n: param
  */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
-	int i;
+	int len, space;
 
 	if (n > 0)
 	{
-		_putchar(' ');
-
-		for (i = 0; i < n ; i++)
+		for (len = 0; len < n; len++)
 		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
 			_putchar('\\');
-			_putchar(' ');
+
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 
+	_putchar('\n');
 }
