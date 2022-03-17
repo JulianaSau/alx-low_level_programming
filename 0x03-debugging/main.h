695300c0a41eabf40adaf6b836e4f2ca52cd72da
Juliana Sau <julienyange@gmail.com>
2022-03-17 06:09:06 -0700

    Create test file for positive_or_negative fn

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+/**
+ * File - main.h
+ *
+ * Description: Contains all function definitions
+ */
 
 void positive_or_negative(int i);
 
-#endif /* MAIN_H */
+#endif
