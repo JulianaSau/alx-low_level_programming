30afba6d5c73d2e5587a1d5795e0cc249bab1243
Sau Juliana <julienyange@gmail.com>
2022-03-17 23:53:08 +0300

    Add program to print a line for non zero input

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line n times followed by a new line
+ *
+ * @n: numbers of times to print line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			break;
+		}
+		else
+		{
+			_putchar(95);
+		}
+
+		_putchar('\n');
+	}
+}
