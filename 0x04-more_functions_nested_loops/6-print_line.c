2fc76b60c8bfcd61495ddbc5abcba421fe90f730
Sau Juliana <julienyange@gmail.com>
2022-03-17 23:57:24 +0300

    Add program to print a line for non zero input

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,17 +12,13 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (n <= 0)
-		{
-			break;
-		}
-		else
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
-
-		_putchar('\n');
 	}
+
+	_putchar('\n');
 }
