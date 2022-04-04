a6e483062cf5292dc530c8d73a47e5ae99a6ca70
Juliana Sau <julienyange@gmail.com>
2022-04-04 07:32:29 -0700

    Add task 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,11 +4,14 @@
 /**
  * main - Prints the name of the program
  * followed by a new line
+ * 
+ * @argc - number of command-line arguments
+ * @argv - string of argumenst passed to main fcn
  *
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
