258ce94b5948bc57357b3b98fb53f53a1c874205
Sau Juliana <julienyange@gmail.com>
2022-04-06 00:28:25 +0300

    Create program that creates an array with character c

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This is a header file that contains function
+ * prototypes that will be used in this project
+ */
+
+char *create_array(unsigned int size, char c);
+
+#endif
