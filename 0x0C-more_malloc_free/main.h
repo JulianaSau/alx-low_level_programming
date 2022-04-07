5d9a67661c84846a595b7369ccb10b13eeabe73d
Sau Juliana <julienyange@gmail.com>
2022-04-07 14:45:06 +0300

    Create header file

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * This is a header file that contains function
+ * prototypes that will be used in this project
+ */
+
+void *malloc_checked(unsigned int b);
+
+#endif
