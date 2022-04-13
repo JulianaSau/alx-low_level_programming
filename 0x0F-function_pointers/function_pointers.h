15c40f7da834544ee6611589bf70dd2cf5b30057
Sau Juliana <julienyange@gmail.com>
2022-04-13 23:53:22 +0300

    Create header file

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * file - header file that contains function pointer prototypes
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
