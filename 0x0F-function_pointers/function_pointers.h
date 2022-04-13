688ffe9be49f5231ec4e8d9a038056d4598056da
Sau Juliana <julienyange@gmail.com>
2022-04-14 01:30:57 +0300

    Create program that prints a name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,10 +2,6 @@
 #define FUNCTION_POINTERS_H
 #include <stdlib.h>
 
-/**
- * file - header file that contains function pointer prototypes
- */
-
 void print_name(char *name, void (*f)(char *));
 
 #endif
