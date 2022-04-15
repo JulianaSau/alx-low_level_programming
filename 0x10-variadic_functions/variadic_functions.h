8f2eb4d4ba801685df1fc7c74d8d46938cfe2cbe
Sau Juliana <julienyange@gmail.com>
2022-04-16 00:32:22 +0300

    Variadic functions tasks

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
-
+#ifndef VAR_HFILE
+#define VAR_HFILE
+/*
+ *filename: variadic_functions.h
+ *Desc: contains prototypes of functions used in this project
+ */
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
 #endif
