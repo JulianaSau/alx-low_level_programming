89b3cfda9d972146e5749a207ca03758a0e9eb07
Juliana Sau <julienyange@gmail.com>
2022-04-25 08:03:11 -0700

    Add fn that frees a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - fn that frees a listint_t list
+ *
+ * @head: ptr to the 1st elem in the list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp_ptr;
+
+	while(head != NULL)
+	{
+		temp_ptr = head->next;
+
+		free(head);
+		head = temp_ptr;
+	}
+}
