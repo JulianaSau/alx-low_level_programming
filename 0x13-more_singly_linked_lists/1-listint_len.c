e4f56c5067499fb56064b367c88a6c1f674c4eb0
Juliana Sau <julienyange@gmail.com>
2022-04-25 06:41:33 -0700

    Add fn that prints no of elemts in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - fn to get the no of elements in a 
+ * singly linked list
+ *
+ * @h: head of a singly linked list
+ *
+ * Return: no of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number_of_nodes = 0;
+
+	while(h != NULL)
+	{
+		number_of_nodes++;
+		h = h->next;
+	}
+
+	return (number_of_nodes);
+}
